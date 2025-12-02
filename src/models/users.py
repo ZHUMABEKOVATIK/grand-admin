ea240ed926a737fe66554312524bee68259c5542
Atabek <atabek.atik.808@gmail.com>
2025-12-02 21:16:22 +0500

    Username & password

--- src/models/users.py
+++ src/models/users.py
@@ -6,3 +6,5 @@ class Users(DefaultDeclarativeBase):
     __tablename__ = "users"
 
     telegram_id: Mapped[int] = mapped_column(BigInteger, unique = True)
+    username: Mapped[str]
+    password: Mapped[str]
